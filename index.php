7786411f463cb49a35308af529b8247611f23cfb
Alex O Skramstad <askramst@silk3.uvm.edu>
2015-09-25 13:59:58 -0400

    Added optional parameter

--- index.php
+++ index.php
@@ -1,20 +1,33 @@
 <?php
 
-Print"<p>" . Greetings(0300) . '</p>';
+Print Greetings(1300 , "True");
 
 
-function Greetings($hour) {
+function Greetings($hour, $test = '') {
     $hours = (int) $hours;
-    {
-        if ($hour >= 0000 && $hour < 1200)
-            return 'Good Morning';
+    
+        if ($hour >= 0000 && $hour < 1200){
+           if($test = 'True'){
+           return "<h>" .'Good Morning' . "</h>"; }
+           else{
+               return "Good Morning";
+           }
     }
     if ($hour >= 1200 && $hour < 1700) {
-        return 'Good Afternoon';
+         if($test = 'True'){
+           return "<h>" .'Good Afernoon' . "</h>"; }
+           else{
+               return "Good Afternoon";
+           }
     }
+    
     if ($hour > 1700) {
-        return'Good Evening';
+         if($test = 'True'){
+           return "<h>" .'Good Evening' . "</h>"; }
+           else{
+               return "Good Evening"; }
     }
 }
+
 ?>
 
